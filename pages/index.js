c3951a5b788774e58f52a5f0ce112b347419c9bb
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-31 23:54:48 +0600

    finished ch-08 vid-06--> implemented fetch() request and successfully written into feedback.json file

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,33 @@ import { useRef } from "react";
 function HomePage() {
   const emailInputRef = useRef();
   const feedbackInputRef = useRef();
-  function submitFormHandler(event){
+
+  function submitFormHandler(event) {
     event.preventDefault();
 
     const enteredEmail = emailInputRef.current.value;
     const feedback = feedbackInputRef.current.value;
+
+    const requestBody = { email: enteredEmail, feedback: feedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
+
   return (
     <div>
       <h1>Home Page</h1>
       <form onSubmit={submitFormHandler}>
         <div>
           <label htmlFor="email">Email Address</label>
-          <input type="email" id="email" ref={emailInputRef}/>
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div>
           <label htmlFor="feedback">Feedback</label>
