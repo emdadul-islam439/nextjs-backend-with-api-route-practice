da61e00164b3681b95501b6467eb029048371d3c
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-31 23:38:51 +0600

    finished ch-08 vid-10--> written code for handling POST request into /api/feedback end-point

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,5 +1,26 @@
-function handler(req, res){
-    res.status(200).json({message: "All is well"})
+import path from "path";
+import fs from "fs";
+
+function handler(req, res) {
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const feedbackText = req.body.feedback;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      text: feedbackText,
+    };
+
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: "Created!" });
+  } else {
+    res.status(200).json({ message: "All is well" });
+  }
 }
 
 export default handler;
