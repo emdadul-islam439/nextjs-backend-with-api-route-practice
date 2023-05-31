b5670ca4dd6f6f7568fdb9d45f21af4be9831c0a
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-31 23:24:06 +0600

    finished ch-08 vid-04--> added a form with 'ref' and 'form-handler'

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
-function HomePage(){
-    return <h1>Home Page</h1>
+import { useRef } from "react";
+
+function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+  function submitFormHandler(event){
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const feedback = feedbackInputRef.current.value;
+  }
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input type="email" id="email" ref={emailInputRef}/>
+        </div>
+        <div>
+          <label htmlFor="feedback">Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Submit Feedback</button>
+      </form>
+    </div>
+  );
 }
 
 export default HomePage;
