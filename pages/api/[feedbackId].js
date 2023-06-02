57a8dd96286ef7f96a23fa96f426b281f9a56690
Emdadul_Islam <emdadulislam439@gmail.com>
2023-06-02 11:24:40 +0600

    finished ch-08 vid-09--> learned about creating the dynamic api end-point

---
+++ pages/api/[feedbackId].js
@@ -0,0 +1,12 @@
+import { buildFeedbackPath, extractFeedback } from "./feedback";
+
+function handler(req, res) {
+  const feedbackId = req.query.feedbackId;
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+  console.log("data: " + JSON.stringify(data));
+  const selectedFeedback = data.find((feedback) => feedback.id === feedbackId);
+  res.status(200).json({ feedback: selectedFeedback });
+}
+
+export default handler;
--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,12 +1,31 @@
+import { Fragment, useState } from "react";
 import { buildFeedbackPath, extractFeedback } from "../api/feedback";
 
 function FeedbackPage(props) {
+  const [loadedFeedback, setLoadedFeedback] = useState();
+  function loadFeedbackHandler(feedbackId) {
+    console.log("loadedFeedbackhandler: " + feedbackId);
+    fetch(`/api/${feedbackId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("data: " + JSON.stringify(data));
+        setLoadedFeedback(data.feedback);
+      });
+  }
   return (
-    <ul>
-      {props.feedbackItems.map((item) => (
-        <li key={item.id}>{item.text}</li>
-      ))}
-    </ul>
+    <Fragment>
+      {loadedFeedback && <p>{loadedFeedback.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            feedback: {item.text}
+            <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 
@@ -15,7 +34,7 @@ export default FeedbackPage;
 export async function getStaticProps(context) {
   const filePath = buildFeedbackPath();
   const data = extractFeedback(filePath);
-
+  console.log("data: " + JSON.stringify(data));
   return {
     props: {
       feedbackItems: data,
